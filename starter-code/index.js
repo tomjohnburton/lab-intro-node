52e4916d2f5e78468267bcb712ba206616299046
tomjohnburton <tomjohnburton@users.noreply.github.com>
2018-10-30 22:28:50 +0100

    Start

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,20 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
+  constructor() {
+    this.item = [];
+    this.length = item.length;
+  }
+  add(item) {
+    for(var i = 0; i < item; i++){
+      this.item.push()
+    }
+  }
   get(pos) {}
   max() {}
   min() {}
   average() {}
   sum() {}
 };
+
+
 
 module.exports = SortedList;
